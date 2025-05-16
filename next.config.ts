8c6b3d47db950dd5c190f8210cbb9d3b0c70de29
Aditi1900 <aditi.v.dharmadhikari@gmail.com>
2025-05-16 00:12:50 -0400

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    domains: ['images.pexels.com', 'public.flourish.studio'],
+  },
+  
+    theme: {
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(-50%)" },
+        },
+      },
+      fontFamily: {
+        montserrat: ['var(--font-montserrat)', 'sans-serif'],
+      },
+       animation: {
+        scroll: "scroll 20s linear infinite",
+      },
+    },
+  },
+}
 
 export default nextConfig;
